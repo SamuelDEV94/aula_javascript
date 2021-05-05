14b42ce8ff023db8cb5e96b15af30216bafef58a
SamuelDEV94 <samuel.silvajr@gmail.com>
2021-05-05 15:40:43 -0300

    introdução JS

---
+++ JS/main.js
@@ -0,0 +1,149 @@
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://bbb.fm/");
+    //window.location.href = "https://bbb.fm/";
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe po mouse aqui";
+}
+
+
+function load(){
+    alert("Pagina carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+
+
+
+
+/*function soma(n1, n2){
+
+    return n1 + n2;
+}*/
+
+
+/*
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua Idade?");
+console.log(validarIdade(idade));
+*/
+
+
+//alert(soma( 5, 10));
+
+
+
+
+
+/*var d = new Date();
+alert(d.getDate());
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+
+
+/*var count;
+for(count=0; count <=5; count++){
+    alert(count);
+};*/
+
+
+
+/*var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+};*/
+
+
+
+/*var idade = prompt("Qual a sua idade");
+if(idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+};
+*/
+
+
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome;
+/*
+
+
+
+
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+
+
+
+
+
+//var lista = [" maça ", " pera ", " laranja "];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var nome = "Samuel Borges";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão e o melhor time do mundo";
+//alert(idade + idade2);
+//alert(nome + " tem " +  idade + " Anos ");
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
